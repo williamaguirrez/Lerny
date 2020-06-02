1458689aeaaaede54b5409f330898d0025dba34c
William Aguirre <aguirrewilliam-1996@hotmail.com>
2020-06-01 19:09:31 -0500

    views Proyectos sociales

--- frontend_vue/src/components/ProyectosSociales.vue
+++ frontend_vue/src/components/ProyectosSociales.vue
@@ -1,8 +1,17 @@
 <template>
     <v-app id="inspire">
+        <!-- Slider Revolution -->
+        <script type="application/javascript" src="https://wtechnology.co/slider/assets/js/includes/jquery/jquery.js"></script>
+        <script type="application/javascript" src="https://wtechnology.co/slider/revslider/public/assets/js/jquery.themepunch.tools.min.js"></script>
+        <script type="application/javascript" src="https://wtechnology.co/slider/revslider/public/assets/js/jquery.themepunch.revolution.min.js"></script>
+        <script type="application/javascript" id="revslider_script" src="https://wtechnology.co/slider/assets/js/revslider.js"></script>
+        <div class="revslider" data-alias="proyectossociales"></div>
         <v-row class="contenedor_proyecSociales">
             <MenuIzquierdo/>
             <MenuSuperior/>
+            <v-row>
+                <h1 style="font-size:50px; padding-left:20px">Todos los Proyectos Sociales de Lerny </h1>
+            </v-row>
             <script type="application/javascript" src="https://apps.elfsight.com/p/platform.js" defer></script>
             <div class="elfsight-app-670d8b13-fc2b-4d94-bdb9-095cc450f8b3"></div>
         </v-row>
@@ -24,8 +33,10 @@
 </script>
 
 <style scoped>
+    @import 'https://wtechnology.co/slider/revslider/public/assets/css/settings.css';
+
     .contenedor_proyecSociales{
-        padding-top: 0px;
+        padding-top: 60px;
         padding-bottom: 20px;
         padding-left: 80px;
         padding-right: 30px;
