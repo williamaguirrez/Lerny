e28c297393f5853c0e4598d09932993efb7df52d
William Aguirre Zapata <aguirrewilliam-1996@hotmail.com>
2020-09-21 10:55:11 -0500

    Update firebase.js

--- frontend_vue/src/firebase.js
+++ frontend_vue/src/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase'
 import firestore from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAV33FlhDRJGsH83oqDdhpxpo2bC1scXNE",
-  authDomain: "me310-sugar.firebaseapp.com",
-  databaseURL: "https://me310-sugar.firebaseio.com",
-  projectId: "me310-sugar",
-  storageBucket: "me310-sugar.appspot.com",
-  messagingSenderId: "1004615747365",
-  appId: "1:1004615747365:web:de1375120b7698a0fe7ccc"
+  apiKey: "*******",
+  authDomain: "*****.firebaseapp.com",
+  databaseURL: "https:/*****.firebaseio.com",
+  projectId: "******",
+  storageBucket: "*****.appspot.com",
+  messagingSenderId: "*******",
+  appId: "*****"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
